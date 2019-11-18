9252b56e523ba0d5f498e249dc8e9644f2333b8a
alanqthomas <alan.q.thomas@gmail.com>
2019-11-18 18:04:05 -0500

    Fix naming

--- src/combineEffects.ts
+++ src/combineEffects.ts
@@ -1,8 +1,8 @@
 import { Dispatch, Action, AnyAction } from 'redux'
 
-import { Effect } from './effector'
+import { Effect } from './effect'
 
-export function combineEffectors<T, A extends Action<T> = AnyAction, S = {}>(effectors: {
+export function combineEffects<T, A extends Action<T> = AnyAction, S = {}>(effectors: {
   [name: string]: Effect<T, A, S>
 }): Effect<T, A, S> {
   return (action: A, dispatch: Dispatch, state: S) => {
--- src/createEffectorMiddleware.ts
+++ src/createEffectorMiddleware.ts
@@ -1,12 +1,12 @@
 import { Dispatch, Action, Middleware, MiddlewareAPI } from 'redux'
 
-import { Effect } from './effector'
+import { Effect } from './effect'
 
 export const createEffectorMiddleware = <T, A extends Action<T>, S>(
-  effector: Effect<T, A, S>
+  effect: Effect<T, A, S>
 ): Middleware<{}, S, Dispatch> => {
   return (api: MiddlewareAPI<Dispatch, S>) => (next: Dispatch) => (action: A): void => {
     next(action)
-    effector(action, api.dispatch, api.getState())
+    effect(action, api.dispatch, api.getState())
   }
 }
